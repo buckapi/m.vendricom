4635a88d6495827a7a3e37476a8bf82a81cb45ed
debian <debian@localhost.localdomain>
2024-11-25 03:03:25 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { GlobalService } from './service/global.service';
+import { AuthPocketbaseService } from './service/auth-pocketbase.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [/* RouterOutlet, */
+    CommonModule,
+    /* HomeComponent */
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'm.vendricom';
+  constructor(public global:GlobalService,
+    public auth:AuthPocketbaseService
+  ){
+}
 }
